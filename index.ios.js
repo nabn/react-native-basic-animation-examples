e18e4e3c803858ea6a550fa9a37b5fccd36991e1
nabn <nabeen.khadka@gmail.com>
2016-08-09 09:15:42 +0545

    square that moves

--- index.ios.js
+++ index.ios.js
@@ -1,27 +1,58 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   AppRegistry,
   StyleSheet,
+  Animated,
+  PanResponder,
   Text,
   View
-} from 'react-native';
+} from 'react-native'
 
-class piggs extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.top}>top</Text>
-        <Text style={styles.mid}>mid</Text>
-        <Text style={styles.bottom}>bottom</Text>
-      </View>
-    );
+const SQUARE_DIMENSIONS = 100
+
+class Piggs extends Component {
+  state = {
+    pan: new Animated.ValueXY()
   }
+
+  getStyle = _ => ([
+    styles.square,
+    { transform: this.state.pan.getTranslateTransform() }
+  ])
+
+  componentWillMount = _ => {
+    this._animatedValueX = 0
+    this._animatedValueY = 0
+    this.state.pan.x.addListener( ({value}) => this._animatedValueX = value )
+    this.state.pan.y.addListener( ({value}) => this._animatedValueY = value )
+
+    this._panResponder = PanResponder.create({
+      onMoveShouldSetResponderCapture: _ => true,
+      onMoveShouldSetPanResponderCapture: _ => true,
+      onPanResponderGrant: (e, gestureState) => {
+        this.state.pan.setOffset({x: this._animatedValueX, y: this._animatedValueY})
+        this.state.pan.setValue({x: 0, y: 0}) // initial Value
+      },
+      onPanResponderMove: Animated.event([
+        null, {dx: this.state.pan.x, dy: this.state.pan.y}
+      ]),
+      onPanResponderRelease: _ => this.state.pan.flattenOffset(),
+    })
+  }
+
+  componentWillUnmount = _ => {
+    this.state.pan.x.removeAllListeners()
+    this.state.pan.y.removeAllListeners()
+  }
+
+  render = _ => (
+    <View style={styles.container}>
+      <Animated.View
+        {...this._panResponder.panHandlers}
+        style={this.getStyle()} />
+    </View>
+  )
+
 }
 
 const styles = StyleSheet.create({
@@ -30,15 +61,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  top: {
-    fontSize: 20,
-  },
-  mid: {
-    fontSize: 20,
-  },
-  bottom: {
-    fontSize: 30,
-  },
-});
+  square: {
+    width: SQUARE_DIMENSIONS,
+    height: SQUARE_DIMENSIONS,
+    backgroundColor: 'lightblue',
+  }
+})
 
-AppRegistry.registerComponent('piggs', () => piggs);
+AppRegistry.registerComponent('piggs', () => Piggs)
